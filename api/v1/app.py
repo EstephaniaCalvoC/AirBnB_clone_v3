61222a08352c81520c84fe6de94be40a8495352f
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-05-08 21:40:03 -0500

    Fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,11 +15,13 @@ def close(self):
     """close session"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
